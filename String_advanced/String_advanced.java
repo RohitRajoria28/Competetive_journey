d25a2cf25c9e1180c5cc271f856de95b38d4c19f
RohitRajoria28 <rrajoria28@gmail.com>
2023-01-03 19:00:07 +0530

    updated

--- String_advanced/String_advanced.java
+++ String_advanced/String_advanced.java
@@ -52,3 +52,45 @@ class Solution {
         return sb.toString();
     }
 }
+
+// 151. Reverse Words in a String
+
+
+class Solution {
+    public String reverseWords(String s) {
+        String arr[]=s.split(" ");
+        int n=arr.length;
+        int i=0;
+        int j=n-1;
+        
+        StringBuilder str=new StringBuilder();
+
+       for(String s: arr){
+            str.append(s);
+       }
+
+        return str.toString();
+    }
+}
+// 
+
+class Solution {
+    public String reverseWords(String s) {
+        String res="";
+         int n=arr.length;
+         for(int i=0;i<n;i++){
+            String curr="";
+
+            while(i<n && s.charAt(i)!=' '){
+                curr+=s.charAt(i);
+                i++;
+            }
+            if(curr.length()>0){
+                res=(curr)+(res.length()==0?"":" ")+res;
+            }
+
+         }
+
+         return res;
+    }
+}
